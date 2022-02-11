c4e8688628b8f5764633ce18f74aee9dcaeef9fa
Aleja Martinez <martinezmayra605@gmail.com>
2022-02-11 17:31:08 -0500

    Archivo que me indica fecha y hora actual

---
+++ src/Dates.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Dates {
+    public static void main(String[] args) {
+        Date fecha= new Date();
+
+        System.out.println("La fecha y hora actual es: "+ fecha);
+    }
+}
