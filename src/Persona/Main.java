f7ee7461be87be86168afb1989de80d61ba9e474
Aleja Martinez <martinezmayra605@gmail.com>
2022-02-11 21:52:46 -0500

    Metodos de la clase persona e instanciacion de objetos persona para probar la clase.

--- src/Persona/Main.java
+++ src/Persona/Main.java
@@ -1,6 +1,4 @@
 package Persona;
-
-import java.nio.charset.Charset;
 import java.util.Scanner;
 
 public class Main {
@@ -10,27 +8,47 @@ public class Main {
         String name = reader.nextLine();
         System.out.println("Ingrese edad de la persona");
         Scanner reader1 = new Scanner(System.in);
-        int age = Integer.parseInt(reader.nextLine());
+        int age = Integer.parseInt(reader1.nextLine());
         System.out.println("Ingrese sexo de la persona M o H");
         Scanner reader2 = new Scanner(System.in);
         char sex = reader2.nextLine().charAt(0);
+        System.out.println(sex);
         System.out.println("Ingrese peso de la persona");
         Scanner reader3 = new Scanner(System.in);
-        double weigth = Double.parseDouble(reader.nextLine());
+        double weigth = Double.parseDouble(reader3.nextLine());
         System.out.println("Ingrese altura de la persona");
         Scanner reader4 = new Scanner(System.in);
-        double height = Double.parseDouble(reader.nextLine());
+        double height = Double.parseDouble(reader4.nextLine());
 
-        Persona person1 = new Persona(name,age,sex,weigth,height
-        );
+        Persona person1 = new Persona(name,age,sex,weigth,height);
+        Persona person2 = new Persona(name,age,sex);
+        Persona person3 = new Persona();
+
+        //estableciendo los valores faltantes de los objetos
+        person2.setPeso(50);
+        person2.setAltura(1.65);
+
+        person3.setEdad(25);
+        person3.setAltura(1.58);
+        person3.setPeso(45);
+        person3.setNombre("Alejandra");
+        person3.setSexo('M');
+
+        //comprobando peso
+        person1.CalcularIMC();
+        person2.CalcularIMC();
+        person3.CalcularIMC();
+
+        //indicando si es mayor de edad
+        person1.esMayorDeEdad();
+        person2.esMayorDeEdad();
+        person3.esMayorDeEdad();
+
+        //mostrando la info de los objetos
+        person1.toString();
+        person2.toString();
+        person3.toString();
+
 
     }
 }
-/*
-
-        Ahora, crea una clase ejecutable que haga lo siguiente:
-        •	Pide por teclado el nombre, la edad, sexo, peso y altura.
-        •	Crea 3 objetos de la clase anterior, el primer objeto obtendrá las anteriores variables pedidas por teclado, el segundo objeto obtendrá todos los anteriores menos el peso y la altura y el último por defecto, para este último utiliza los métodos set para darle a los atributos un valor.
-        •	Para cada objeto, deberá comprobar si está en su peso ideal, tiene sobrepeso o por debajo de su peso ideal con un mensaje.
-        •	Indicar para cada objeto si es mayor de edad.
-        •	Por último, mostrar la información de cada objeto.*/
--- src/Persona/Persona.java
+++ src/Persona/Persona.java
@@ -3,9 +3,10 @@ package Persona;
 public class Persona {
     private String nombre = "";
     private  int edad = 0;
-    private int DNI;
-    private char sexo = 'H';
-    private double peso, altura = 0;
+    private String DNI;
+    private char sexo;
+    private double peso;
+    private double altura;
 
     final static int BP = -1;
     final static int PI = 0;
@@ -14,66 +15,78 @@ public class Persona {
     public Persona(String nombre, int edad,char sexo, double peso, double altura) {
         this.nombre = nombre;
         this.edad = edad;
-        this.sexo = sexo;
+        this.sexo = this.comprobarSexo(sexo);
         this.peso = peso;
         this.altura = altura;
+        this.DNI = this.generaDNI();
     }
     public  Persona(String nombre, int edad, char sexo){
         this.nombre = nombre;
         this.edad = edad;
-        this.sexo = sexo;
+        this.sexo = this.comprobarSexo(sexo);
         this.peso = 0;
         this.altura = 0;
+        this.DNI = this.generaDNI();
+
     }
     public Persona(){
-        System.out.println("Recuerda ponerle despues nombre a esta persona");
+        this.DNI = this.generaDNI();
     }
 
     //metodos
-    public int CalcularIMC(){
+    public int CalcularIMC() {
         double IMC = (this.peso / (Math.pow(this.altura, 2)));
-        if(IMC < 20){
+        if (IMC < 20) {
             System.out.println("Bajo de peso");
             return BP;
-        }else if(IMC >=20 && IMC <= 25){
+        } else if (IMC >= 20 && IMC <= 25) {
             System.out.println("peso Ideal");
             return PI;
-        }else{
+        } else if (IMC > 25) {
             System.out.println("Sobrepeso");
             return SP;
+        }else {
+            System.out.println("No hay datos para sacar el IMC");
+            return 0;
         }
 
     }
+
+
 
     public boolean esMayorDeEdad(){
         if(this.edad >= 18){
             System.out.println("Usted es mayor de edad");
             return true;
         }else{
+            System.out.println("Estas muy chiquis aun");
             return  false;
         }
     }
 
-    private void comprobarSexo(char sexo){
-        if( sexo != 'H' || sexo != 'M'){
-             this.sexo = 'H';
+    private char comprobarSexo(char sexo){
+        switch (sexo){
+            case 'M':
+                return 'M';
+            default: return 'H';
         }
     }
     public String toString(){
-        return "";
+        String mensaje = "Nombre: " + this.nombre + "\nEdad: " + this.edad + "\nSexo: " + this.sexo + "\nPeso: " + this.peso + "\nAltura: " + this.altura + "\nDNI: " + this.DNI;
+        System.out.println(mensaje);
+        return mensaje;
     }
 
-    private  String generaDNI(){
+    private String generaDNI(){
         String Numbers = "";
         String DIN = "";
         for (int i = 0; i < 8; i++) {
-            int number = (int) Math.floor((Math.random()*9));
-            DIN = Numbers.concat(String.valueOf(number));
+            int number = (int) Math.floor((Math.random()*9)); //numero aleatorio
+            char number1 = (char) (number + 65); //lo paso a char
+            DIN = Numbers.concat(Character.toString(number1)); //lo concateno en otra variable
             Numbers = DIN;
-            System.out.println(DIN);
         }
-
-        return DIN ;
+        return DIN;
     }
 
     public void setNombre(String nombre) {
@@ -85,7 +98,7 @@ public class Persona {
     }
 
     public void setSexo(char sexo) {
-        this.sexo = sexo;
+        this.sexo = this.comprobarSexo(sexo);
     }
 
     public void setPeso(double peso) {
